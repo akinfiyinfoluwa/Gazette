eee8d232b9497b5ef367ff6e24a9bcf02a2ce7d7
akinfiyinfoluwa <apostleofwords@gmail.com>
2025-08-15 06:17:34 +0100

    updated the contact page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/contact', title: 'Contact' },
   { href: '/about', title: 'About' },
 ]
 
