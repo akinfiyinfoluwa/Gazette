5514ffdab0909fee697d55256ac4f9f78960e6fd
akinfiyinfoluwa <apostleofwords@gmail.com>
2025-08-17 15:23:23 +0100

    updated the homepage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = ({ posts }) => {
   }
 
   return (
-    <div className="relative h-96 w-full overflow-hidden">
+    <div className="relative h-96 w-full overflow-hidden rounded-lg">
       {posts.map((post, index) => (
         <div
           key={post.slug}
@@ -35,9 +35,9 @@ const Hero = ({ posts }) => {
             objectFit="cover"
             className="h-full w-full"
           />
-          <div className="absolute inset-0 bg-black bg-opacity-50" />
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-            <h2 className="text-4xl font-bold">{post.title}</h2>
+          <div className="absolute inset-0 rounded" />
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-white bg-gradient-to-tl from-gray-800 to-transparent bg-opacity-50 p-6">
+            <h2 className="text-3xl font-bold px-1.5">{post.title}</h2>
             <Link href={`/blog/${post.slug}`} legacyBehavior>
               <a className="mt-4 rounded-full bg-primary-500 px-6 py-2 text-lg font-semibold hover:bg-primary-600">
                 Read More
